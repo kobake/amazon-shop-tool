524afa40c13fac09bc60afcee2b0253c5ddf8c11
kobake <kobake@users.sourceforge.net>
2014-09-07 04:44:00 +0900

    年対応

--- js/index.js
+++ js/index.js
@@ -71,41 +71,91 @@ function step1(){
 }
 
 // サインイン後の画面（注文履歴）
+var years = [];
 function step2(){
-	console.log("-- step2 --");
-	// 記録
-	page.evaluate(function(){
-		jQuery('.action-box').each(function(){
-			console.log("--------------------");
-			var item = jQuery(this);
-			console.log(item.find('.order-level .info-data a').text().trim()); // 注文番号
-			console.log(item.find('.order-level h2').text().trim()); // 日付
-			console.log(item.find('.item-title').text().trim()); // タイトル
-			console.log(item.find('.order-level .price').text().trim()); // 価格
-			console.log(item.find('.order-level .top-text').text().trim()); // 状態
-			console.log(item.find('.order-level .info-data.recipient').text().trim()); // 受取人
-			console.log(item.find('.product-group-name').text().trim()); // Kindle or not
+	// 候補年リスト
+	years = page.evaluate(function(){
+		var years = [];
+		jQuery('#orderFilter option').each(function(){
+			var year = jQuery(this).text().trim();
+			if(year === '2014年')return;
+			if(year.match(/年$/)){
+				years.push(year);
+			}
 		});
+		return years;
 	});
-	// 次の条件
-	page.onLoadFinished = function () {
-		step2();
-	};
-	// 次へ
-	var next = page.evaluate(function(){
-		// 次へ
-		var next = jQuery('div.pagination-box a:contains("次へ")');
-		if(next.size() == 1){
-			location.href = next.attr('href');
-			return true;
-		}
-		else{
-			return false;
-		}
-	});
-	if(!next){
-		// 終わり
+
+	// 最初の年について処理 (それ以降の年は再帰呼び出しで発行)
+	step3(0, 0);
+}
+
+// サインイン後の画面（注文履歴）
+function step3(year_index, page_index){
+	console.log("-- step3 --");
+
+	// すべての年の処理が完了していたらプログラム終了
+	if(year_index >= years.length){
 		phantom.exit(0);
+	}
+
+	// 年
+	var year = years[year_index];
+
+	// 年選択
+	if(page_index === 0){
+		// 年ラベル
+		console.log("========== " + year + " ==========");
+
+		// 次の条件
+		page.onLoadFinished = function () {
+			step3(year_index, page_index + 1);
+		};
+
+		// 年選択
+		page.evaluate(function(year){
+			jQuery('#orderFilter').val(year);
+			jQuery('#order-dropdown-form input[type="submit"]').click();
+		}, year);
+	}
+	// ページめくり
+	else{
+		// 記録
+		page.evaluate(function(){
+			jQuery('.action-box').each(function(){
+				console.log("--------------------");
+				var item = jQuery(this);
+				console.log(item.find('.order-level .info-data a').text().trim()); // 注文番号
+				console.log(item.find('.order-level h2').text().trim()); // 日付
+				console.log(item.find('.item-title').text().trim()); // タイトル
+				console.log(item.find('.order-level .price').text().trim()); // 価格
+				console.log(item.find('.order-level .top-text').text().trim()); // 状態
+				console.log(item.find('.order-level .info-data.recipient').text().trim()); // 受取人
+				console.log(item.find('.product-group-name').text().trim()); // Kindle or not
+			});
+		});
+
+		// 次の条件
+		page.onLoadFinished = function () {
+			step3(year_index, page_index + 1);
+		};
+
+		// 次ページ
+		var next = page.evaluate(function(){
+			// 次ページ
+			var next = jQuery('div.pagination-box a:contains("次へ")');
+			if(next.size() == 1){
+				location.href = next.attr('href');
+				return true;
+			}
+			else{
+				return false;
+			}
+		});
+		if(!next){
+			// 次の年
+			step3(year_index + 1, 0);
+		}
 	}
 }
 
